762e7c5f84a0ada05b2911854da16c92444effbe
QiangXianSen <1594417377@qq.com>
2017-11-29 14:36:08 +0800

    [issue 3:【Add】 修改配置文件]

--- config-client/src/main/java/com/example/demo/controller/HelloController.java
+++ config-client/src/main/java/com/example/demo/controller/HelloController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-    @Value("${democonfigclient.message}")
+    @Value("${spring.cloud.config.name}")
     private String hello;
 
     @RequestMapping("/hello")
