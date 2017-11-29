679254b42337d0ab3ddfb3e4878c67ca43853008
QiangXianSen <1594417377@qq.com>
2017-11-29 10:19:58 +0800

    [issue 1:【Add】 新增项目]

---
+++ config-client/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright @ 2017北京思特奇信息技术股份有限公司。 All rights reserved.
+ *
+ * @author: wangqiang
+ * @Date: 2017/11/17 15:28
+ */
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright @ 2017北京思特奇信息技术股份有限公司。 All rights reserved.
+ * @Title HelloController
+ * @Project spring-cloud-demo
+ * @Description TODO
+ * @author wangqiang
+ * @date 2017/11/17 15:28
+ */
+@RestController
+public class HelloController {
+    @Value("${democonfigclient.message}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
